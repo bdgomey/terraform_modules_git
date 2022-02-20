4b7eb9bb8d0050d61f53d6f44394cae7d1712c9f
Brian <brian.james.gomes@gmail.com>
2022-02-20 10:09:59 -0600

    s

--- Networking_Module/variables.tf
+++ Networking_Module/variables.tf
@@ -1,36 +1,50 @@
+variable "vnet_name" {
+  description = "Name of the vnet to create"
+  type        = string
+  default     = "acctvnet"
+}
+
 variable "resource_group_name" {
   description = "Name of the resource group to be imported."
   type        = string
 }
-variable "network_security_group_name" {
-  description = "Name of the network security group"
-  type        = string
-}
-variable "vnet_name" {
-  description = "Name of the vnet."
-  type        = string
-}
-variable "vnet_location" {
-  description = "vnet location"
-  type        = string
-}
+
 variable "address_space" {
-  description = "vnet address space."
   type        = list(string)
+  description = "The address space that is used by the virtual network."
+  default     = ["10.0.0.0/16"]
 }
-variable "subnet_names" {
-  description = "subnet names"
-  type        = list(string)
-}
-variable "subnet_prefixes" {
-  description = "subnet prefixes"
-  type        = list(string)
-}
+
+# If no values specified, this defaults to Azure DNS 
 variable "dns_servers" {
   description = "The DNS servers to be used with vNet."
   type        = list(string)
   default     = []
 }
+
+variable "subnet_prefixes" {
+  description = "The address prefix to use for the subnet."
+  type        = list(string)
+  default     = ["10.0.1.0/24"]
+}
+
+variable "subnet_names" {
+  description = "A list of public subnets inside the vNet."
+  type        = list(string)
+  default     = ["subnet1", "subnet2", "subnet3"]
+}
+
 variable "tags" {
-  type = map(string)
+  description = "The tags to associate with your network and subnets."
+  type        = map(string)
+
+  default = {
+    ENV = "test"
+  }
+}
+
+variable "vnet_location" {
+  description = "The location of the vnet to create. Defaults to the location of the resource group."
+  type        = string
+  default     = null
 }
