e102e0247b8ed9efa92a69467ad04ce7254e848d
Brian <brian.james.gomes@gmail.com>
2022-02-20 10:03:21 -0600

    s

--- Networking_Module/main.tf
+++ Networking_Module/main.tf
@@ -4,14 +4,14 @@ data "azurerm_resource_group" "networking_rg" {
 
 resource "azurerm_network_security_group" "vnet" {
   name                = var.network_security_group_name 
-  resource_group_name  = data.azurerm_resource_group.vnet.name
+  resource_group_name  = data.azurerm_resource_group.networking_rg.name
   location = data.azurerm_resource_group.vnet.location
 }
 
 resource "azurerm_virtual_network" "vnet" {
   name                = var.vnet_name
-  resource_group_name  = data.azurerm_resource_group.vnet.name
-  location = var.vnet_location != null ? var.vnet_location: data.azurerm_resource_group.vnet.location
+  resource_group_name  = data.azurerm_resource_group.networking_rg.name
+  location = var.vnet_location != null ? var.vnet_location: data.azurerm_resource_group.networking_rg.location
   address_space       = var.address_space
   dns_servers         = var.dns_servers
 
@@ -21,7 +21,7 @@ resource "azurerm_virtual_network" "vnet" {
 resource "azurerm_subnet" "vnet" {
   count = length(var.subnet_names)
   name                 = var.subnet_names[count.index]
-  resource_group_name  = data.azurerm_resource_group.vnet.name
+  resource_group_name  = data.azurerm_resource_group.networking_rg.name
   virtual_network_name = azurerm_virtual_network.vnet.name
   address_prefixes     = [var.subnet_prefixes[count.index]]
 }
