c3f3f4299c19f85875a535703a8e2b2475052abb
Brian <brian.james.gomes@gmail.com>
2022-02-20 09:17:26 -0600

    commits

--- Networking_Module/main.tf
+++ Networking_Module/main.tf
@@ -2,38 +2,26 @@ data "azurerm_resource_group" "vnet" {
   name = var.resource_group_name
 }
 
+resource "azurerm_network_security_group" "vnet" {
+  name                = var.network_security_group_name 
+  resource_group_name  = data.azurerm_resource_group.vnet.name
+  location = data.azurerm_resource_group.vnet.location
+}
+
 resource "azurerm_virtual_network" "vnet" {
-  name = var.vnet_name
-  resource_group_name = data.azurerm_resource_group.vnet.name
-  location = var.vnet_location != null ? var.vnet_location : data.azurerm_resource_group.vnet.location
-  address_space = var.address_space
-  dns_servers = var.dns_servers
+  name                = var.vnet_name
+  resource_group_name  = data.azurerm_resource_group.vnet.name
+  location = var.vnet_location != null ? var.vnet_location: data.azurerm_resource_group.vnet.location
+  address_space       = var.address_space
+  #dns_servers         = var.dns_servers
+
   tags = var.tags
 }
 
-resource "azurerm_subnet" "subnet" {
+resource "azurerm_subnet" "vnet" {
   count = length(var.subnet_names)
-  name = var.subnet_names[count.index]
-  resource_group_name = data.azurerm_resource_group.vnet.name
+  name                 = var.subnet_names[count.index]
+  resource_group_name  = data.azurerm_resource_group.vnet.name
   virtual_network_name = azurerm_virtual_network.vnet.name
-  address_prefix = [var.subnet_prefixes[count.index]]
-}
-
-locals {
-  azurerm_subnets = {
-    for index, subnet in azurerm_subnet.subnet :
-    subnet.name => subnet.id
-  }
-}
-
-resource "azurerm_subnet_network_security_group_association" "vnet" {
-  for_each                  = var.nsg_ids
-  subnet_id                 = local.azurerm_subnets[each.key]
-  network_security_group_id = each.value
-}
-
-resource "azurerm_subnet_route_table_association" "vnet" {
-  for_each       = var.route_tables_ids
-  route_table_id = each.value
-  subnet_id      = local.azurerm_subnets[each.key]
+  address_prefixes     = var.subnet_prefixes[count.index]
 }
