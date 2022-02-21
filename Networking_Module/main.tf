0b5863373975fc70f4e384206f2214226afc3e43
Brian <brian.james.gomes@gmail.com>
2022-02-20 21:11:49 -0600

    s

--- Networking_Module/main.tf
+++ Networking_Module/main.tf
@@ -19,8 +19,8 @@ resource "azurerm_subnet" "subnet" {
   for_each = var.subnets
   resource_group_name = var.resource_group_name
   virtual_network_name = azurerm_virtual_network.vnet.name
-  name = each.key.name
-  address_prefixes = each.key.address_prefix
+  name = each.value.name
+  address_prefixes = each.value.address_prefix
 }
 
 resource "azurerm_public_ip" "bastion_pip" {
