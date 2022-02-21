b83f525afbc1c14c3e6ea93a569582d09e5d5a22
Brian <brian.james.gomes@gmail.com>
2022-02-20 21:04:54 -0600

    s

--- Networking_Module/outputs.tf
+++ Networking_Module/outputs.tf
@@ -18,3 +18,14 @@ output "vnet_address_space" {
   value       = azurerm_virtual_network.vnet.address_space
 }
 
+output "azure_subnet_id" {
+    value = {
+        for id in keys(var.subnets) : id => azurerm_subnet.subnet[id].id
+    }
+    description = "Lists the ID's of the subnets"
+}
+
+output "bastion_pubip" {
+  value = azurerm_public_ip.bastion_pip.ip_address
+  description = "List the public IP of the bastion server"
+}
